c770cc9f2bbf476ea057349c303af86929900e4c
Tomasdfen <tomasdfen@gmail.com>
2020-12-21 21:39:58 +0100

    Me cago en la mar salada

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 //Librerias
 
-const contenido = document.getElementById('content');
-const contenido_basico = '\\assets\\contenido_basico.html'
-
+let stored_vars = ['x']
+// Funciones
 function escribe(html_path, element){
   fetch(html_path)
   .then(response => response.text())
@@ -13,7 +12,25 @@ function escribe(html_path, element){
 
 
 function init(){
+  const contenido_basico = '\\assets\\contenido_basico.html';
   escribe(contenido_basico, contenido);
 }
 
+function adjust_vars(parametro){
+  const texto = `<div class="row p-2">
+     <label class="col-md" for="${parametro}">${parametro}</label>
+     <input class="col-sm-4" style="text-align:right" type="text" onchange="update_var(this)" id="${parametro}" value="">
+     </div>`;
+}
+
+function update_var(element){
+  values.set(this.id, this.value);
+}
+
+
+
+const contenido = document.getElementById('content');
+
 init();
+
+
