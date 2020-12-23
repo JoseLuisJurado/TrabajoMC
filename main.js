838ad8d54a92067e055368ecec056a657c8c1d89
Tomasdfen <tomasdfen@gmail.com>
2020-12-23 04:47:53 +0100

    Draw funcional y renombrado a plot

--- main.js
+++ main.js
@@ -10,81 +10,87 @@ var x_min = 0;
 var x_max = 10;
 var y_min = 0;
 var y_max = 10;
+var x0 = 2;
+var go_button = document.body;
 
 // Funciones
-// function escribe(html_path, element) {
-//   fetch(html_path)
-//     .then((response) => response.text())
-//     .then((data) => {
-//       element.innerHTML = data;
-//     });
-// }
 
-// function init() {
-//   const contenido_basico = "\\assets\\contenido_basico.html";
-//   escribe(contenido_basico, contenido);
-// }
+function print_mode(html_path, element) {
+  fetch(html_path)
+    .then((response) => response.text())
+    .then((data) => {
+      element.innerHTML = data;
+    });
+}
 
-// function adjust_vars(parametro){
-//   const texto = `<div class="row p-2">
-//      <label class="col-md" for="${parametro}">${parametro}</label>
-//      <input class="col-sm-4" style="text-align:right" type="text" onchange="update_var(this)" id="${parametro}" value="">
-//      </div>`;
-// }
-
-// function update_var(element){
-//   values.set(this.id, this.value);
-// }
+function init() {
+  const contenido_basico = "\\assets\\contenido_basico.html";
+  print_mode(contenido_basico, contenido)
+  setTimeout(grab_vars, 100);
+  setTimeout(plot, 100);
+  go_button.addEventListener('click', function(){
+    grab_vars();
+    plot();
+   } );
+}
 
 function grab_vars() {
   stored_equation = document.getElementById("eq").value;
   stored_vars = document.getElementById("vars").value.split(",");
   iterations = document.getElementById("n").value;
   final_iterations = document.getElementById("m").value;
-  x_min = document.getElementById("x-min").value;
-  x_max = document.getElementById("x-max").value;
-  y_min = document.getElementById("y-min").value;
-  y_max = document.getElementById("y-max").value;
+  x_min = parseFloat(document.getElementById("x-min").value);
+  x_max = parseFloat(document.getElementById("x-max").value);
+  y_min = parseFloat(document.getElementById("y-min").value);
+  y_max = parseFloat(document.getElementById("y-max").value);
+  x0 = document.getElementById("x0").value;
+  go_button = document.getElementById('go_button');
 }
 
 // function plot_attractor(){
   
 // }
 
-// $(function() {
-//     $("#vars").change(function() {
-//       console.log(this);
-//     });
-// });
+function orbita(){
+  const expr = math.compile(stored_equation)
 
+  // evaluate the expression repeatedly for different values of x
+  fn = expr.evaluate({x:x0})
+  var xs = [x0]
+  var ys = [fn];
+  var n;
+  if (final_iterations != 0){
+    console.log("la hemos liado")
+    n = final_iterations-iterations
 
-function draw() {
-  try {
+  }
+  else{
+    n = iterations
+  }
+  for (let i = 0; i < n; i++) {
+    xs.push(fn)
+    fn = expr.evaluate({x:fn})
+    ys.push(fn)
+  }
+  return [xs,ys]
+}
+function plot() {
     // compile the expression once
-    const expr = math.compile(stored_equation);
+    const expr = math.compile(stored_equation)
 
     // evaluate the expression repeatedly for different values of x
-    const xValues = math.range(x_min, x_max, 0.5).toArray();
-    const yValues = math.range(y_min, y_max, 0.5).toArray();
-    const Values = xValues.map(function (x) {
-      return yValues.map(function(y){
-        return expr.evaluate({"x":x,"y":y});
-      });
-    });
+    xs = orbita()[0]
+    ys = orbita()[1]
 
     // render the plot using plotly
     const trace1 = {
-      x: xValues,
-      y: yValues,
-      z: Values,
-      type: "scatter",
-    };
-    
-    const data = [trace1];
-    Plotly.newPlot("plot", data);
-  
-  } catch (err) {
-    console.error(err);
-    alert(err);
-  }
+      x: xs,
+      y: ys,
+      type: 'scatter'
+    }
+    const data = [trace1]
+    Plotly.newPlot('plot', data)
+
 }
+
+init()
